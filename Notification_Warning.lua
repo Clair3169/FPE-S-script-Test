a22f3011326e8a546ee38b553b1f5385629f71c4
ClaireExploiting <yisusjj27@gmail.com>
2025-11-09 23:48:54 -0400

    Create Notification_Warning.lua

---
+++ Notification_Warning.lua
@@ -0,0 +1,6 @@
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Warning!";
+    Text = "Be careful, this action cannot be undone";
+    Icon = ""; -- Puedes poner un ID de imagen si quieres (por ejemplo: "rbxassetid://123456789")
+    Duration = 5; -- Duración en segundos
+})
