39e80d52a041dcd8b925c0afc05df673c7b8eaef
ClaireExploiting <yisusjj27@gmail.com>
2025-11-10 07:36:19 -0400

    Update Notification_Warning.lua

--- Notification_Warning.lua
+++ Notification_Warning.lua
@@ -1,6 +1,18 @@
+local SoundService = game:GetService("SoundService")
+
+local warningSound = Instance.new("Sound")
+warningSound.SoundId = "rbxassetid://1570162306" -- <-- ¡Recuerda cambiar este ID!
+warningSound.Parent = SoundService
+
+warningSound:Play()
+
+warningSound.Ended:Connect(function()
+    warningSound:Destroy()
+end)
+
 game.StarterGui:SetCore("SendNotification", {
     Title = "Warning!";
     Text = "Be careful, this action cannot be undone";
-    Icon = ""; -- Puedes poner un ID de imagen si quieres (por ejemplo: "rbxassetid://123456789")
-    Duration = 5; -- Duración en segundos
+    Icon = ""; 
+    Duration = 5; 
 })
