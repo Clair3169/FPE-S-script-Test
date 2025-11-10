3b809cf63e4558f9c54dbfd828fb4bf311efa11d
ClaireExploiting <yisusjj27@gmail.com>
2025-11-10 00:19:50 -0400

    Update Cframe_Walkspeed.lua

--- Cframe_Walkspeed.lua
+++ Cframe_Walkspeed.lua
@@ -7,7 +7,7 @@ local RunService = game:GetService("RunService")
 local player = Players.LocalPlayer
 
 -- ==== CONFIGURACIÓN ====
-local FakeSpeed = 120           -- velocidad visual (aparente)
+local FakeSpeed = 127           -- velocidad visual (aparente)
 local MoveSmoothness = 0.15    -- 0.05 = muy rápido, 0.25 = más suave
 -- ========================
 
