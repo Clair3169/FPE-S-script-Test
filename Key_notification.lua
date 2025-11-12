ad3fb62c6243902b4223d0d2dcf0e0b8f407453c
ClaireExploiting <yisusjj27@gmail.com>
2025-11-12 06:14:16 -0400

    Update Key_notification.lua

--- Key_notification.lua
+++ Key_notification.lua
@@ -8,16 +8,29 @@ hasThirdPerson.Name = "ThirdPersonEnabled"
 hasThirdPerson.Value = false
 hasThirdPerson.Parent = player
 
+local SoundService = game:GetService("SoundService")
+local warningSound = Instance.new("Sound")
+warningSound.SoundId = "rbxassetid://8382337318" -- <-- ¡Recuerda cambiar este ID!
+warningSound.Parent = SoundService
+
 local bindableFunction = Instance.new("BindableFunction")
+
+warningSound:Play()
+
+warningSound.Ended:Connect(function()
+    warningSound:Destroy()
+end)
+
 bindableFunction.OnInvoke = function(buttonClicked)
 	if buttonClicked == "Yess!!" then
 		hasThirdPerson.Value = true
 		-- Tercera persona
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Clair3169/FPE-S-script-Test/refs/heads/main/3rd_Person.lua", true))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Clair3169/FPE-S-script/refs/heads/main/3rd_Person.lua", true))()
 		-- Shift Lock
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Clair3169/FPE-S-script-Test/refs/heads/main/Shiftlock.lua", true))()
-		-- Cframe speed
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Clair3169/FPE-S-script-Test/refs/heads/main/Notification_CframeSpeed.lua", true))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Clair3169/FPE-S-script/refs/heads/main/Shiftlock.lua", true))()
+		-- Cframe Walkspeed 
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Clair3169/FPE-S-script/refs/heads/main/Notification_Walkspeed.lua", true))()
+		
 	elseif buttonClicked == "Nha" then
 		hasThirdPerson.Value = false
 	end
