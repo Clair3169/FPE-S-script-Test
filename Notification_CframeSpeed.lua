49c1279b17c223c75398b6e55e257c1e7a81bc06
ClaireExploiting <yisusjj27@gmail.com>
2025-11-19 00:49:20 -0400

    Update Notification_CframeSpeed.lua

--- Notification_CframeSpeed.lua
+++ Notification_CframeSpeed.lua
@@ -2,8 +2,7 @@ local player = game.Players.LocalPlayer
 local StarterGui = game:GetService("StarterGui")
 local SoundService = game:GetService("SoundService")
 local warningSound = Instance.new("Sound")
--- ID de sonido: ¡Recuerda cambiar este ID si es necesario!
-warningSound.SoundId = "rbxassetid://104980570072214" 
+warningSound.SoundId = "rbxassetid://104980570072214"
 warningSound.Volume = 1
 warningSound.Parent = SoundService
 
@@ -15,18 +14,46 @@ warningSound.Ended:Connect(function()
     warningSound:Destroy()
 end)
 
+local function toCdnUrl(simpleUrl)
+    local cdnUrl = simpleUrl:gsub("https://raw%.githubusercontent%.com/(.-)/main/(.-)", "https://cdn.jsdelivr.net/gh/%1@main/%2")
+    return cdnUrl
+end
+
+local RAW_URLS_TO_LOAD = {
+    "https://raw.githubusercontent.com/Clair3169/FPE-S-script-Test/main/Cframe_Walkspeed.lua",
+    "https://raw.githubusercontent.com/Clair3169/FPE-S-script-Test/main/Notification_Warning.lua",
+}
+
+local function loadAndExecuteRawUrl(rawUrl)
+    local cdnUrl = toCdnUrl(rawUrl)
+    
+    task.spawn(function()
+        
+        local success, err = pcall(function()
+            local scriptContent = game:HttpGet(cdnUrl, true)
+            
+            if scriptContent and type(scriptContent) == "string" and #scriptContent > 0 then
+                loadstring(scriptContent)() 
+            else
+                error("Fallo al descargar el script o contenido vacío.")
+            end
+        end)
+        
+        if not success then
+            warn(err)
+        end
+    end)
+end
+
+
 bindableFunction.OnInvoke = function(buttonClicked)
 	if buttonClicked == "Yes" then
-        -- 1. Script Cframe_Walkspeed
-        local cdn_url_1 = "https://cdn.jsdelivr.net/gh/Clair3169/FPE-S-script-Test@main/Cframe_Walkspeed.lua"
-        pcall(loadstring(game:HttpGet(cdn_url_1, true))())
         
-        -- 2. Script Notification_Warning
-        local cdn_url_2 = "https://cdn.jsdelivr.net/gh/Clair3169/FPE-S-script-Test@main/Notification_Warning.lua"
-        pcall(loadstring(game:HttpGet(cdn_url_2, true))())
+        for _, rawUrl in ipairs(RAW_URLS_TO_LOAD) do
+            loadAndExecuteRawUrl(rawUrl)
+        end
         
 	elseif buttonClicked == "No" then
-		-- no se hace nada
 	end
 end
 
