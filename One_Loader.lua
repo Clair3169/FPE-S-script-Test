7d705eb89420ad10064cd20b040da66c1a088c51
ClaireExploiting <yisusjj27@gmail.com>
2026-03-06 04:36:43 -0400

    Update One_Loader.lua by modifying URL list
    Removed some URLs from the loader script while retaining others.

--- One_Loader.lua
+++ One_Loader.lua
@@ -31,9 +31,7 @@ local urls_raw = {
 	"https://raw.githubusercontent.com/Clair3169/FPE-S-script-Test/refs/heads/main/Anti_VoidPart.lua",
 	"https://raw.githubusercontent.com/Clair3169/FPE-S-script-Test/refs/heads/main/Auto-Block.lua",
 	"https://raw.githubusercontent.com/Clair3169/FPE-S-script-Test/refs/heads/main/Auto_Health.lua",
-	"https://raw.githubusercontent.com/Clair3169/FPE-S-script-Test/refs/heads/main/Auto_BuyItem.lua",
-	"https://raw.githubusercontent.com/Clair3169/FPE-S-script-Test/refs/heads/main/EventChristmas.lua",
-	"https://raw.githubusercontent.com/Clair3169/FPE-S-script-Test/refs/heads/main/Luz.lua"
+	"https://raw.githubusercontent.com/Clair3169/FPE-S-script-Test/refs/heads/main/Auto_BuyItem.lua"
 }
 
 -- ----------------------------------------------------
